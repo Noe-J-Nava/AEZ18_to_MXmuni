926a4e2d47e02af702a1f40d1c8d87355a37434f
Noe-J-Nava <97967131+Noe-J-Nava@users.noreply.github.com>
2022-12-14 13:18:00 -0600

    :bug:

--- code/Mxmap_AEZ18shp_creation.R
+++ code/Mxmap_AEZ18shp_creation.R
@@ -10,6 +10,8 @@ rm(list = ls())
 library(tidyverse)
 library(raster)
 library(rgdal)
+library(rgeos)
+library(sp)
 
 # Shapefiles
 ## All of the AEZ around the globe
@@ -19,7 +21,6 @@ AEZ_map <- readOGR(dsn = 'assets/GTAPAEZ_v10',
 AEZ_map_reg <- readOGR(dsn = 'assets/GTAPAEZ_v10',
                        layer = 'GTAPv10_reg')
 AEZ_map_reg <- AEZ_map_reg[AEZ_map_reg$GTAP == "MEX",]
-
 
 ## Mexico divided by municipalities
 MXmap_muni <- readOGR(dsn = 'assets/muni',
@@ -32,35 +33,55 @@ MXmap_AEZ18 <- raster::intersect(AEZ_map_reg, AEZ_map)
 MXmap_muni <- spTransform(MXmap_muni,
                           CRS(MXmap_AEZ18@proj4string@projargs))
 MXmap_muni$muni_id <- paste0(MXmap_muni$CVE_ENT, MXmap_muni$CVE_MUN)
-MXmap <- raster::intersect(MXmap_AEZ18, MXmap_muni)
-
+MXmap_sp <- raster::intersect(MXmap_AEZ18, MXmap_muni)
 # Some final data wrangling
-MXmap@data <- MXmap@data %>%
+MXmap_sp@data <- MXmap_sp@data %>%
   dplyr::select(aez, CVE_ENT, NOMGEO, muni_id) %>%
   rename(AEZ_id = aez) %>%
   rename(ENT_id = CVE_ENT) %>%
   rename(muni_name = NOMGEO)
 
+# Creating output data frame
+MXmap <- MXmap_muni@data
+MXmap <- MXmap %>%
+  dplyr::select(CVE_ENT, NOMGEO, muni_id) %>%
+  #rename(AEZ_id = aez) %>%
+  rename(ENT_id = CVE_ENT) %>%
+  rename(muni_name = NOMGEO)
+MXmap$id <- as.character(0:(length(MXmap$ENT_id)-1))
+
+# Creating and identifying points
+MXpnts_muni <- gCentroid(MXmap_muni, byid = TRUE)
+#plot(USpnts_cnty)
+MXpnts_muni <- sp::over(MXpnts_muni, MXmap_AEZ18)
+MXpnts_muni$id <- as.character(0:(length(MXpnts_muni$aez)-1))
+MXpnts_muni <- MXpnts_muni %>%
+  dplyr::select(aez, id)
+
+# Merging
+MXmap <- left_join(MXmap, MXpnts_muni, by = "id")
+MXmap <- MXmap %>% dplyr::select(!id) %>% rename(AEZ_id = aez)
+
 # Some checks
-## Number of AEZ within Mexico (should be 12)
-length(unique(MXmap@data$AEZ_id)) == length(unique(MXmap_AEZ18$aez))
+## Number of AEZ within Mexico (should be 12 + 1 for NAs)
+### Notice threee municipalities were not assigned a AEZ.
+length(unique(MXmap$AEZ_id)) == length(unique(MXmap_AEZ18$aez)) + 1
 ## Check if same ids
-all(unique(MXmap@data$AEZ_id) %in% unique(MXmap_AEZ18$aez))
-all(unique(MXmap_AEZ18$aez) %in% unique(MXmap@data$AEZ_id))
+all(c(unique(MXmap$AEZ_id), NA) %in% c(unique(MXmap_AEZ18$aez), NA))
+all(c(unique(MXmap_AEZ18$aez), NA) %in% c(unique(MXmap$AEZ_id), NA))
 
 ## Number of municipalities in Mexico (should be 2471)
-length(unique(MXmap@data$muni_id)) == length(unique(MXmap_muni$muni_id))
+length(unique(MXmap$muni_id)) == length(unique(MXmap_muni$muni_id))
 ## Check if same ids
-all(unique(MXmap@data$muni_id)[-3] %in% unique(MXmap_muni$muni_id))
-all(unique(MXmap_muni$muni_id) %in% unique(MXmap@data$muni_id))
+all(unique(MXmap$muni_id) %in% unique(MXmap_muni$muni_id))
+all(unique(MXmap_muni$muni_id) %in% unique(MXmap$muni_id))
 
 # Save shapefile
-writeOGR(MXmap, 
+writeOGR(MXmap_sp, 
          dsn = 'output/MXmap_AEZ18',
          layer = 'MXmap_AEZ18',
          driver = 'ESRI Shapefile')
 # Save Excel file with mapping of municipalities into AEZs in Mexico
-MXmuni_id <- MXmap@data
-write_csv(MXmuni_id, file = 'output/AEZ18_to_MXmuni_id.csv')
-saveRDS(MXmuni_id, file = 'output/AEZ18_to_MXmuni_id.rds')
+write_csv(MXmap, file = 'output/AEZ18_to_MXmuni_id.csv')
+saveRDS(MXmap, file = 'output/AEZ18_to_MXmuni_id.rds')
 # end
